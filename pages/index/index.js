6425f04d2150f5fab861406ff6e4097e757b0615
airbreak <jiangjianming_m@163.com>
2016-11-06 01:27:17 +0800

    finish menu switch

--- pages/index/index.js
+++ pages/index/index.js
@@ -17,12 +17,22 @@ Home.prototype={
 
 //获取应用实例
 var app = getApp()
-var navArr=['推荐','居家','餐具','配件','服装','洗护','婴童','杂货','饮食','其它']
+var navArr=[{title:'推荐',index:0},
+{title:'居家',index:1},
+{title:'餐具',index:2},{title:'配件',index:3},
+{title:'服装',index:4},{title:'洗护',index:5},
+{title:'婴童',index:6},{title:'杂货',index:7},
+{title:'饮食',index:8},{title:'其它',index:9}]
+var classArr=['selected'];
 var myHome=new Home()
 Page({
   data: {
     navArr:navArr,
-    currentIndex:0
+    viewClassArr:['show'],
+    classArr:classArr,
+    currentItem:'推荐',
+    colorsArr:['#6F5499','#1E69AD','#59AF50','#563D7C',
+    '#CF4646','#3998DB','#51A67B','#EFD65B','#653A54','#5BB69A']
   },
   //事件处理函数
   bindViewTap: function() {
@@ -42,8 +52,11 @@ Page({
     })
   },
   selecteClass:function(event){
-    var target=event.target,
-         index = myHome.getIndexForNavMenu(target.dataset.titleName)
+     var target=event.target,
+         index = myHome.getIndexForNavMenu(target.dataset.titleName);
+      this.setData({
+        currentItem:target.dataset.titleName
+      })
   }
 })
 
--- pages/major/major.js
+++ pages/major/major.js
@@ -5,7 +5,7 @@ Page({
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     ],
-    indicatorDots: false,
+    indicatorDots: true,
     autoplay: false,
     interval: 5000,
     duration: 1000
